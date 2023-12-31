e66cceec4d551dc476da1f2d8c55e0990ac6c2b0
webshakil <chumkysk@gmail.com>
2024-01-01 01:21:10 +0600

    initialsetup with tailwindcss

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-cyan-900">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default page
